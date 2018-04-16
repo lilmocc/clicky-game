0aa2c1e9d1034b1f41873d732f407adf8df39c64
lilmocc <lissa.prudencio@gmail.com>
2018-04-15 23:31:10 -0700

    work in progress

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Navbar = props => (
+  <div>
+    <h2>Clicky Game</h2>
+    <h4>Score:</h4>
+  </div>
+);
+
+export default Navbar;
---
+++ src/components/Title/Title.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Title = props => (
+  <div class="jumbotron">
+    <h1 class="display-4">Clicky Game!</h1>
+    <p class="lead">Click on an image to earn points, but don't click on any more than once!</p>
+
+  </div>
+);
+
+export default Title;
